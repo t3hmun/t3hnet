dfb1e0966cf0b552db9225c2c22f43e9874b89d5
mun <git@t3hmun.com>
2019-06-08 17:30:16 +0100

    Minimal ASP project.
    Well as minimal as possible using WebHostBuilder.

---
+++ t3hnet/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace t3hnet
+{
+    using Microsoft.AspNetCore.Hosting;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var webHostBuilder = CreateWebHostBuilder(args);
+            var webHost = webHostBuilder.Build();
+            webHost.Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args)
+        {
+            var builder = new WebHostBuilder();
+            builder.UseKestrel();
+            builder.UseStartup<Startup>();
+            return builder;
+        }
+    }
+}
---
+++ t3hnet/Startup.cs
@@ -0,0 +1,19 @@
+﻿namespace t3hnet
+{
+    using Microsoft.AspNetCore.Builder;
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.Extensions.DependencyInjection;
+
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.Run(async context => { await context.Response.WriteAsync("Salutations"); });
+        }
+    }
+}
